8313605215478232b3f148f86a02fb520c7c0229
Gaston Aganza <gaston.aganza@gmail.com>
2020-06-11 20:02:01 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,32 @@
+www.where-to-drop.com
+
+## Where to Drop - Warzone
+
+Simple script that runs on `!ping` or `!p` command that randomly selects a location to drop in Call of Duty - Warzone.
+
+## List of Available Drop Locations
+
+- Downtown
+- Park
+- Hospital
+- TV Station
+- Dam
+- Military Base
+- Quarry
+- Lumber
+- Stadium
+- Port
+- Hills
+- Promenade East
+- Promenade West
+- Airport
+- Boneyard
+- Superstore
+- Storage Town
+- Prison
+- Farmland
+- Easter Egg: Just insta drop
+
+## Download
+
+[Link to add bot to your discord server.](https://discordapp.com/oauth2/authorize?client_id=704184701115695165&scope=bot&permissions=5120)
---
+++ src/bot.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const LOCATIONS = require("./constants").LOCATIONS;
+
+let auth;
+try {
+  auth = require("./auth.json");
+} catch (error) {
+  auth = { token: process.env.TOKEN };
+}
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomLocation() {
+  return LOCATIONS[randomInteger(0, LOCATIONS.length - 1)];
+}
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (message) => {
+  if (message.content.substring(0, 1) == "!") {
+    let args = message.content.substring(1).split(" ");
+    let cmd = args[0].toLowerCase();
+    args = args.splice(1);
+
+    switch (cmd) {
+      case "p":
+      case "ping":
+        message.reply(getRandomLocation());
+    }
+  }
+});
+
+client.login(auth.token);
